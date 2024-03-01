111bf707aae465c81493751cbcded80a2efd972a
hotequil <hotequil@protonmail.com>
2024-02-29 22:39:13 -0300

    feat: init Link component

---
+++ src/components/Link/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const StyledLink = styled.a`
+    color: ${({ theme }) => theme.colors.primary};
+    text-decoration: underline;
+    width: fit-content;
+`
