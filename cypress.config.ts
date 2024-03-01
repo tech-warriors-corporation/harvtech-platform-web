c9ee2d86d3db623bbc40b049b5e0d495c2a4fcc6
hotequil <hotequil@protonmail.com>
2024-02-29 22:20:25 -0300

    chore: configure tests for components and pages with Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'cypress'
+import dotenv from 'dotenv'
+import path from 'path'
 
 export default defineConfig({
     e2e: {
-        setupNodeEvents() {},
+        setupNodeEvents(on, config) {
+            const environmentVariables = dotenv.config({ path: path.resolve('./.env') }).parsed
+
+            config.baseUrl = environmentVariables.VITE_BASE_URL
+
+            return config
+        },
     },
     component: {
         devServer: {
