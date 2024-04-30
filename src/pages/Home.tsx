98c8bae3cff95fb39a800aa928919c99a88c6f77
hotequil <hotequil@protonmail.com>
2024-04-29 21:34:51 -0300

    feat: use Wrapper in Login and Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+import { Wrapper } from '~/components/Wrapper'
+
+export const Home: React.FC = () => <Wrapper>TODO: create Home page</Wrapper>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+import { Wrapper } from '~/components/Wrapper'
+
+export const Login: React.FC = () => <Wrapper>TODO: create Login page</Wrapper>
