5544229bd414c3883a74ed31c99745056bab8bc8
hotequil <hotequil@protonmail.com>
2024-06-28 16:15:50 -0300

    feat: add new variables for theme

--- src/theme.ts
+++ src/theme.ts
@@ -11,8 +11,16 @@ export const theme: DefaultTheme = {
         two: '4px',
         three: '8px',
         four: '16px',
+        five: '24px',
+        six: '32px',
     },
     times: {
         short: '200ms',
+    },
+    breakpoints: {
+        full: '1760px',
+        large: '1244px',
+        medium: '836px',
+        small: '580px',
     },
 }
