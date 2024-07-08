5facfc9732c4b32731e3e8416ddb6f849d31b9fd
hotequil <hotequil@protonmail.com>
2024-07-07 21:04:36 -0300

    feat: remove opacity from Tooltip

--- src/components/Tooltip/styles.ts
+++ src/components/Tooltip/styles.ts
@@ -7,7 +7,6 @@ export const StyledTooltipAnchor = styled(Ariakit.TooltipAnchor)`
 `
 
 export const StyledTooltip = styled(Ariakit.Tooltip)`
-    display: block !important;
     padding: ${({ theme }) => `${theme.spaces.two} ${theme.spaces.three}`};
     font-size: 0.75rem;
     font-weight: 400;
@@ -18,10 +17,5 @@ export const StyledTooltip = styled(Ariakit.Tooltip)`
     box-shadow: ${({ theme: { colors, spaces } }) => `0 1px ${spaces.one} 0 ${colors.lightGray}`};
     cursor: default;
     z-index: 50;
-    transition: opacity ${({ theme }) => theme.times.short};
-    opacity: 0;
-
-    &[data-open='true'] {
-        opacity: 1;
-    }
+    text-align: center;
 `
