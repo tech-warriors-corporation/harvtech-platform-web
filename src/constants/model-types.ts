b6c7b32e7147469580326b5d07193c8e1298fab5
hotequil <hotequil@protonmail.com>
2024-08-03 12:16:45 -0300

    feat: create modelType based on ModelType enum

---
+++ src/constants/model-types.ts
@@ -0,0 +1,3 @@
+import { ModelType } from '~enums/ModelType'
+
+export const modelTypes = Object.values(ModelType)
--- src/enums/ModelType.ts
+++ src/enums/ModelType.ts
@@ -1,3 +1,6 @@
 export enum ModelType {
     RICE_LEAF = 'rice_leaf',
+    BEAN_LEAF = 'bean_leaf',
+    POTATO_LEAF = 'potato_leaf',
+    TOMATO_LEAF = 'tomato_leaf',
 }
